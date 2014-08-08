fb1a6f03b8f315492e67bed03d878d6dd474ccf1
ramanaallu <ramana.allu@ektha.com>
2014-08-08 12:09:07 +0530

    Revert "after changes made to helloworld.java file"
    This reverts commit 20ea60a5d69a48f2be1456e1ec17eaeb6168b8de.

--- src/com/mkyong/ws/HelloWorld.java
+++ src/com/mkyong/ws/HelloWorld.java
@@ -10,7 +10,6 @@ public class HelloWorld{
  
 	@WebMethod(operationName="getHelloWorld")
 	public String getHelloWorld(String name) {
-		System.out.println("Hello World JAX-WS "+name);
 		return "Hello World JAX-WS " + name;
 	}
 	
