4e169ceca3bc0c9c5ff8c28b29592bc3c88191a8
ramanaallu <ramana.allu@ektha.com>
2014-08-08 12:01:27 +0530

    WebServicesExample

---
+++ src/com/mkyong/ws/HelloWorld.java
@@ -0,0 +1,22 @@
+package com.mkyong.ws;
+ 
+import java.util.ArrayList;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+ 
+@WebService
+public class HelloWorld{
+ 
+	@WebMethod(operationName="getHelloWorld")
+	public String getHelloWorld(String name) {
+		return "Hello World JAX-WS " + name;
+	}
+	
+	
+	public int getHello()
+	{
+		return 1;
+	}
+ 
+}
